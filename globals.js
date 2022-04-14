f9a68c900e093ab1ebc6ac396111fa4179d55579
pearlarora <ipearlarora@gmail.com>
2022-04-15 05:17:20 +0530

    globals

---
+++ globals.js
@@ -0,0 +1,5 @@
+console.log(__dirname); //Path to current directory
+console.log(__filename); //File name
+console.log(require); //Function to use modules(CommonJS)
+console.log(module); //Info about current module(File)
+console.log(process); //Info about the env where the program is being executed
