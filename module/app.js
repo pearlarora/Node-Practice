82f7dc73716d3b47c904af5d1fa9b20669abf150
pearlarora <ipearlarora@gmail.com>
2022-04-16 04:40:34 +0530

    modules

---
+++ module/app.js
@@ -0,0 +1,8 @@
+//Moduled: Encapsulated code(only shares minimal)
+
+const names = require("./variables");
+const sayHi = require("./utils");
+
+sayHi(names.john);
+sayHi(names.peter);
+sayHi("Pearl");
