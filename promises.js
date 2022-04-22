c443151a3204eca3549813b48f1f05d083432080
pearlarora <ipearlarora@gmail.com>
2022-04-22 14:39:22 +0530

    promises in fs

--- promises.js
+++ promises.js
@@ -1,23 +1,32 @@
-const { readFile } = require("fs");
+const { readFile, writeFile } = require("fs").promises;
+// const util = require("util");
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
-const getText = (path) => {
-  //Using promises
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf-8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+// const getText = (path) => {
+//   //Using promises
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf-8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// };
 
 //Using async await
 const start = async () => {
   try {
-    const first = await getText("./content/first.txt");
-    console.log(first);
+    // const first = await readFilePromise("./content/first.txt", "utf-8");
+    const first = await readFile("./content/first.txt", "utf-8");
+    const second = await readFile("./content/second.txt", "utf-8");
+    await writeFile(
+      "./content/result.txt",
+      `Writing using writeFilePromise: ${first}, ${second}`
+    );
+    console.log(first, second);
   } catch (error) {
     console.log(error);
   }
