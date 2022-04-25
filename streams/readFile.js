3d76186024c44684df4092954fba51e11dc56106
pearlarora <ipearlarora@gmail.com>
2022-04-25 20:43:07 +0530

    streams readFile

--- streams/readFile.js
+++ streams/readFile.js
@@ -1,9 +1,13 @@
 const { createReadStream } = require("fs");
-const stream = createReadStream("./content/big.txt", {
+const stream = createReadStream("./content/bigF.txt", {
   highWaterMark: 90000, //Control size of buffer
   encoding: "utf8",
 });
 
 stream.on("data", (result) => {
   console.log(result);
+});
+
+stream.on("error", (err) => {
+  console.log(err);
 });
